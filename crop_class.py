f515af1c612884017c53a6da7bdcf30a99461c41
kerslaket <44108@longroad.ac.uk>
2015-06-11 12:16:06 +0100

    :)

--- crop_class.py
+++ crop_class.py
@@ -1,4 +1,5 @@
 #crop_class
+import random
 
 class Crop:
     """A generic food crop"""
@@ -9,20 +10,112 @@ class Crop:
         self._days_growing = 0
         self._growth_rate = growth_rate
         self._light_need = light_need
-        self._water_meed = water_need
+        self._water_need = water_need
         self._status = "Seed"
         self._type = "Generic"
 
         #the underscores indicate that the attributes are private
 
+    def needs(self):
+        return{"Light Need":self._light_need,"Water Need":self._water_need}
+
+    def report(self):
+        return {"Type":self._type,"Status":self._status,"Growth":self._growth,"Days Growing":self._days_growing}
+
+    def grow(self,light,water):
+        if light >= self._light_need and water >= self._water_need:
+            self._growth += self._growth_rate
+        self._days_growing += 1
+        self._update_status()
+
+    def _update_status(self):
+        if self._growth > 15:
+            self._status = "Old"
+        elif self._growth > 10:
+            self._status = "Mature"
+        elif self._growth > 5:
+            self._status = "Young"
+        elif self._growth > 0:
+            self._status = "Seedling"
+        elif self._growth == 0:
+            self._status = "Seed"
+
+def auto_grow(crop, days):
+    for day in range(days):
+        light = random.randint(1,10)
+        water = random.randint(1,10)
+        crop.grow(light, water)
+
+def manual_grow(crop):
+    valid = False
+    while not valid:
+        try:
+            light = int(input("Please enter a light value(1-10): "))
+            if 1 <= light <= 10:
+                valid = True
+            else:
+                print("Value entered not valid - please enter a value between 1 and 10")
+        except ValueError:
+            print("Value entered not valid - please enter a value between 1 and 10")
+    valid = False
+    while not valid:
+        try:
+            water = int(input("Please enter a water value(1-10): "))
+            if 1 <= water <= 10:
+                valid = True
+            else:
+                print("Value entered not valid - please enter a value between 1 and 10")
+        except ValueError:
+            print("Value entered not valid - please enter a value between 1 and 10")   
+    crop.grow(light,water)
+
+def display_menu():
+    print("1. Grow manually over 1 day")
+    print("2. Grow manually over 30 days")
+    print("3. Report status")
+    print("0. Exit test program")
+    print()
+    print("Please select an option from teh above menu")
+
+def get_menu_choice():
+    option_valid = False
+    while not option_valid:
+        try:
+            choice = int(input("Option Selected: "))
+            if 0 <= choice <= 4:
+                option_valid = True
+            else:
+                print("Please enter a valid option")
+        except ValueError:
+            print("Please enter a valid option")
+    return choice
+
+def manage_crop(crop):
+    print("This is the crop management program")
+    print()
+    noexit = True
+    while noexit:
+        display_menu()
+        option = get_menu_choice()
+        print()
+        if option == 1:
+            manual_grow(crop)
+            print()
+        elif option == 2:
+            auto_grow(crop,30)
+            print()
+        elif option == 3:
+            print(crop.report())
+        elif option == 0:
+            noexit = False
+            print()
+    print("Thank You for using this program")
+
+     
 def main():
     #instantiate the class
     new_crop = Crop(1,4,3)
-    print(new_crop._status)
-    print(new_crop._light_need)
-    new_crop2 = Crop(1,5,7)
-    print(new_crop2._status)
-    print(new_crop2._light_need)
+    manage_crop(new_crop)
 
-if __name__=="__main__":
+if __name__== "__main__":
     main()
---
+++ potato_class.py
@@ -0,0 +1,9 @@
+from crop_class import *
+
+class Potato(Crop):
+    """A potato crop"""
+
+    def __init__(self):
+        super().__init__(1.3.6)
+
+    
